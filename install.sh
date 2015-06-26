827dfbfa6fb33cee44b66093101a415a2cafbf4a
Kirill Dragalov <kirill@aimfund.ru>
2015-06-26 13:58:50 +0300

    Initial commit

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+function link_if_missing()
+{
+  SRC=$1
+  DEST=$2
+
+  if ! [ -L $DEST ]; then
+    ln -ivs $SRC $DEST
+  else
+    echo "Skipping, link already exists: $SRC"
+  fi
+}
+
+DIR=$(dirname $0)
+(cd $DIR; git submodule update --init)
+
+link_if_missing $DIR/vim          $HOME/.vim
+link_if_missing $DIR/gitconfig    $HOME/.gitconfig
+link_if_missing $DIR/vimrc        $HOME/.vimrc
+link_if_missing $DIR/bashrc       $HOME/.bashrc
+link_if_missing $DIR/bash_profile $HOME/.bash_profile
+
+mkdir -p $HOME/.vim/undo
+vim +PluginInstall +qall
